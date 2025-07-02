95a8f3219924eb4fe4be19a60d02f92bb05bddf6
LarissaGloria <0000094220@senaimgaluno.com.br>
2025-07-02 19:23:22 -0300

    feature: criação da página inicial

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,15 @@
 export default function Home() {
   return (
     <>
+    <box className="h-screen w full clas flex flex-col items-center justify-center gap-20px background-color-gray-200">
+
+    <h1 className="fonte-36px color-gray200 font-bold">Prova Versionamento - RPV Senai Cataguases</h1>
+    <p className="fonte-24px color-">Larissa da Glória de Souza</p>
+    <p>Cataguases, 02 de Julho de 2025</p>
+
+    </box>
+
+
     </>
   )
 }
