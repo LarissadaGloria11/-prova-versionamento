53e5c185feaac893c0ef8aa2e3f7b0a684dc51d4
LarissaGloria <0000094220@senaimgaluno.com.br>
2025-07-02 19:43:29 -0300

    feature: criação da página de contato

---
+++ src/app/contato/page.jsx
@@ -0,0 +1,16 @@
+'use client'
+
+
+export default function PaginaContato() {
+  return (
+    <>
+    <home className="class h-screen w full flex flex-col items-center justify-center">
+
+        <h1>Seja bem-vindo à página de Contato</h1>
+
+    </home>
+
+
+    </>
+  )
+}
